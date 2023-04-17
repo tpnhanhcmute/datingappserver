6cf6b95b79d2bc882dd8f0af7a3c3be4fbb6627e
tpnhan12a1 <tpnhan12a1@gmail.com>
2023-04-17 20:22:45 +0700

    fix user models

--- src/api/model/user.model.ts
+++ src/api/model/user.model.ts
@@ -3,10 +3,13 @@ export interface user {
     email: String,
     password: String,
     fullName: String,
-    nickName:String,
+    career:String,
     dateOfBirth: Date,
     age:Number,
+    hobby:Array<String>,
     gender:String,
+    isAuth:Boolean,
+    isFirstLogin:Boolean,
     occupation:String|null
     locationID: String|null
   }
