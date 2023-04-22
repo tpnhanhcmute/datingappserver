5471bddcf4feb325d52832a4b3084d02e38050f0
tpnhan12a1 <tpnhan12a1@gmail.com>
2023-04-23 02:20:02 +0700

    make it better

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,13 @@
 import express from "express";
 import * as dotenv from "dotenv";
-import { sendEmail } from "./api/services/firebase.service";
 import userRouter from "./router/user.router";
 import locationRouter from "./router/location.router";
-import { getLocation } from "./api/services/location.service";
 import imagesRouter from "./router/images.router";
 import authenticationRouter from "./router/authentication.router"
 
 dotenv.config();
-const PORT: number = parseInt(process.env.PORT as string, 8000);
+let PORT = parseInt(process.env.PORT,10)
+
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -18,9 +17,7 @@ app.use("/users", userRouter);
 app.use("/location", locationRouter);
 app.use("/images", imagesRouter);
 app.use("",authenticationRouter)
-app.get("/", (req,res)=>{
-  res.status(200).send("Hello world");
-} )
+
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
