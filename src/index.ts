094b940f207b16a6cc6a3df24f7a12c747fad03d
tpnhan12a1 <tpnhan12a1@gmail.com>
2023-04-22 19:03:57 +0700

    add authen

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import userRouter from "./router/user.router";
 import locationRouter from "./router/location.router";
 import { getLocation } from "./api/services/location.service";
 import imagesRouter from "./router/images.router";
+import authenticationRouter from "./router/authentication.router"
 
 const app = express();
 app.use(express.json());
@@ -15,6 +16,7 @@ app.use(express.json());
 app.use("/users", userRouter);
 app.use("/location", locationRouter);
 app.use("/images", imagesRouter);
+app.use("",authenticationRouter)
 
 app.get("/", (req, res) => {
   res.send("Hello Huy dep zai!");
