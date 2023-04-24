f57a002a74bf168d64378c0b7748a04ca234f2d1
huyquang <huywocker92016@gmail.com>
2023-04-24 18:43:59 +0700

    merge

--- src/api/model/user.model.ts
+++ src/api/model/user.model.ts
@@ -1,28 +1,27 @@
 export class user {
-    phoneNumber: String
-    email: String
-    password: String
-    fullName: String
-    career:String
-    dateOfBirth: Date
-    age:Number
-    hobby:Array<String>
-    gender:String
-    isAuth:Boolean 
-    isFirstLogin:Boolean 
-    occupation:String|null 
-    constructor(){
-      this.phoneNumber = new String()
-      this.email = new String()
-      this.password = new String()
-      this.fullName = new String()
-      this.career = new String()
-      this.dateOfBirth = new Date()
-      this.age = new Number()
-      this.hobby = new Array<String>()
-      this.gender = new String()
-      this.isAuth = false
-      this.isFirstLogin = false,
-      this.occupation = new String()
-    }
+  phoneNumber: String;
+  email: String;
+  password: String;
+  fullName: String;
+  career: String;
+  dateOfBirth: Date;
+  age: Number;
+  hobby: Array<String>;
+  gender: String;
+  isAuth: Boolean;
+  isFirstLogin: Boolean;
+  occupation: String | null;
+  constructor() {
+    this.phoneNumber = new String();
+    this.email = new String();
+    this.password = new String();
+    this.fullName = new String();
+    this.career = new String();
+    this.dateOfBirth = new Date();
+    this.age = new Number();
+    this.hobby = new Array<String>();
+    this.gender = new String();
+    this.isAuth = false;
+    (this.isFirstLogin = false), (this.occupation = new String());
   }
+}
