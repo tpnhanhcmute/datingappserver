bafd2b18814e93668c2e537f94a1f3976813d55a
huyquang <huywocker92016@gmail.com>
2023-04-24 19:48:59 +0700

    merge

--- src/router/location.router.ts
+++ src/router/location.router.ts
@@ -1,7 +1,9 @@
 import express from "express";
 
 import locationController from "../api/controllers/location.controler";
+
 const router = express.Router();
+
 router.post("/update", locationController.update);
 
 export = router;
