051066de57c441cdfa54439bf660b52ea2696d0a
PainCodermax <trungafk0806@gmail.com>
2023-04-27 20:28:13 +0700

    fix bug

---
+++ build/api/dto/converstation.dto.js
@@ -0,0 +1,11 @@
+// import { User } from "../model/user.model"
+// export class converstation{
+//     // id:String 
+//     user:User
+//     urlimage:String
+//     constructor(){
+//         // this.id  = new String()
+//         this.urlimage = new String()
+//         this.user = 
+//     }
+// }
